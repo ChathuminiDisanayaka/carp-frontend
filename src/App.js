fe6e1c52d1e9593341796ada5a26a5f00d12642f
Rumesha Disanayaka <2019cs017@stu.ucsc.lk>
2023-08-08 10:26:01 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {Switch, Typography} from '@mui/material'
+import Product from "./component/Product";
+import Header from "./component/Header";
+import {Route, Router} from "@mui/icons-material";
+import Footer from "./component/Footer";
+import ProductView from "./component/ProductView";
+import ProductAdd from "./component/ProductAdd";
+import Landing from "./component/Landing";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      // <Router>
+      //     <div>
+      //         <Header />
+      //         <Switch>
+      //             <Route exact path="/" component={Product} />
+      //             <Route path="/view" component={ProductView} />
+      //             <Route path="/add" component={ProductAdd} />
+      //         </Switch>
+      //         <Footer />
+      //     </div>
+      // </Router>
+
+      <div>
+          <Header/>
+          <Landing/>
+          <Product/>
+          <Footer/>
+          <ProductView/>
+          <ProductAdd/>
+      </div>
+
   );
 }
 
