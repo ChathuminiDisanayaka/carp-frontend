0176373a017bcd62a0dc99a70f68e3fec6070daf
damishN <damishnisal100@gmail.com>
2023-08-16 20:47:17 +0530

    reslove routes and change image url : not in images here

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import {Switch, Typography} from '@mui/material'
 import Product from "./component/Product";
 import Header from "./component/Header";
-import {Route, Router} from "@mui/icons-material";
 import Footer from "./component/Footer";
 import ProductView from "./component/ProductView";
 import ProductAdd from "./component/ProductAdd";
 import Landing from "./component/Landing";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
-      // <Router>
-      //     <div>
-      //         <Header />
-      //         <Switch>
-      //             <Route exact path="/" component={Product} />
-      //             <Route path="/view" component={ProductView} />
-      //             <Route path="/add" component={ProductAdd} />
-      //         </Switch>
-      //         <Footer />
-      //     </div>
-      // </Router>
+      <BrowserRouter>
+          <div>
+              <Header />
+              <Switch>
+                  <Route exact path="/" component={Landing} />
+                  <Route exact path="/products" component={Product} />
+                  <Route path="/view" component={ProductView} />
+                  <Route path="/add" component={ProductAdd} />
+              </Switch>
+              <Footer />
+          </div>
+      </BrowserRouter>
 
-      <div>
-          <Header/>
-          <Landing/>
-          <Product/>
-          <Footer/>
-          <ProductView/>
-          <ProductAdd/>
-      </div>
+      // <div>
+      //     <Header/>
+      //     <Landing/>
+      //     <Product/>
+      //     <Footer/>
+      //     {/*<ProductView/>*/}
+      //     <ProductAdd/>
+      // </div>
 
   );
 }
